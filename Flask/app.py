c01838bee9cc163dcfad41943bf483c75d63d7a1
Poojita Karchalkar <karchalkarpoojita@gmail.com>
2024-02-06 11:40:00 +0000

    test

--- Flask/app.py
+++ Flask/app.py
@@ -1,19 +1,2 @@
-from flask import Flask, render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/analysis')
-def analysis():
-    return render_template('analysis.html')
-
-@app.route('/results')
-def results():
-    return render_template('results.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
