0f341f4463d9e02b18f1f0de324566fa7d1a91e0
Poojita Karchalkar <karchalkarpoojita@gmail.com>
2024-02-21 21:30:48 +0000

    file transferrred to flask folder

---
+++ Flask/database.py
@@ -0,0 +1,30 @@
+import psycopg2
+def setup():
+    host = "localhost"
+    port = "5432"    
+    user = "postgres"
+    password = "Poojita15$"
+    database = "populationgenetics"  
+    try:
+        # Establish a connection
+        connection = psycopg2.connect(
+        host=host,
+        port=port,        
+        user=user,        
+        password=password,
+        database=database 
+        )
+        cursor = connection.cursor()
+
+        return cursor, connection
+    
+    except psycopg2.Error as e:
+        print(f"Error: Unable to connect to the PostgreSQL server. {e}")
+
+# Close the cursor and connection   
+def close(cursor, connection):
+    cursor.close()
+    connection.close()
+    print("Connection closed.")
+
+    return
